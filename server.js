062ac9599369e54c82a91369bed1766baa17eeb1
voomoo <101200128+Rakibul-GDN@users.noreply.github.com>
2022-08-31 12:17:11 +0600

    5. Connecting to the Database with Mongoose

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 //Route Files
 const bootcamps = require("./routes/bootcamps");
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+//Connect to database
+connectDB();
 
 const app = express();
 
@@ -14,9 +18,16 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT,
     console.log(
         `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
     )
 );
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //close server and exit process
+    server.close(() => process.exit(1));
+});
