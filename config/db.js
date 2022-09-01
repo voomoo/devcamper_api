c2bca06222475aba412f9559c3afb617b36d6836
voomoo <101200128+Rakibul-GDN@users.noreply.github.com>
2022-09-01 09:56:00 +0600

    6. Creating Our First Model

--- config/db.js
+++ config/db.js
@@ -5,7 +5,9 @@ const connectDB = async () => {
         useNewUrlParser: true,
     });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(
+        `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+    );
 };
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const colors = require("colors");
 
 //Route Files
 const bootcamps = require("./routes/bootcamps");
@@ -13,6 +14,9 @@ connectDB();
 
 const app = express();
 
+//Body parser
+app.use(express.json());
+
 //Mount Routers
 app.use("/api/v1/bootcamps", bootcamps);
 
@@ -21,13 +25,14 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(
     PORT,
     console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+            .bold
     )
 );
 
 //Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${err.message}`.red.bold);
     //close server and exit process
     server.close(() => process.exit(1));
 });
