58c1a1d77c7509a9f576d80bb95b8cfddacbd1f2
voomoo <101200128+Rakibul-GDN@users.noreply.github.com>
2022-09-06 10:52:32 +0600

    22. Populate, Virtuals, Cascade Delete

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -28,7 +28,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     );
 
     //Finding resources
-    query = Bootcamp.find(JSON.parse(queryStr));
+    query = Bootcamp.find(JSON.parse(queryStr)).populate("courses");
 
     //Select Fields
     if (req.query.select) {
@@ -140,7 +140,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 //route DELETE /api/v1/bootcamps/:id
 //@access Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    const bootcamp = await Bootcamp.findById(req.params.id);
 
     if (!bootcamp) {
         return next(
@@ -150,6 +150,8 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
             )
         );
     }
+
+    bootcamp.remove();
 
     res.status(200).json({
         success: true,
