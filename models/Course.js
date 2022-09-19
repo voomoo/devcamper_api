cce23687f3fcb34492d0409b5c712718325496ef
voomoo <101200128+Rakibul-GDN@users.noreply.github.com>
2022-09-19 11:57:17 +0600

    25. Aggregate - Calculating the Average Course

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,36 @@ const CourseSchema = new mongoose.Schema({
     },
 });
 
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId },
+        },
+        {
+            $group: {
+                _id: "$bootcamp",
+                averageCost: { $avg: "$tuition" },
+            },
+        },
+    ]);
+
+    try {
+        await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
+        });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+//Call the getAverageCost after save
+CourseSchema.post("save", function () {
+    this.constructor.getAverageCost(this.bootcamp);
+});
+
+//Call the getAverageCost before remove
+CourseSchema.pre("remove", function () {
+    this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model("Course", CourseSchema);
