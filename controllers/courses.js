7a0916ab8fa215d831ba00325481fbf6771d9c62
voomoo <101200128+Rakibul-GDN@users.noreply.github.com>
2022-09-19 11:41:43 +0600

    24. Update Delete Course

--- controllers/courses.js
+++ controllers/courses.js
@@ -73,3 +73,46 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
         data: course,
     });
 });
+
+//@desc Update Course
+//route Put /api/v1/courses/:id
+//@access Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+    let course = await Course.findById(req.params.id);
+
+    if (!course) {
+        return next(
+            new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+        );
+    }
+
+    course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        data: course,
+    });
+});
+
+//@desc Delete Course
+//route DELETE /api/v1/courses/:id
+//@access Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+    const course = await Course.findById(req.params.id);
+
+    if (!course) {
+        return next(
+            new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+        );
+    }
+
+    await course.remove();
+
+    res.status(200).json({
+        success: true,
+        data: {},
+    });
+});
