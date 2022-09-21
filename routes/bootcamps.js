520cdf0c880d1420feb43341ceff55bb5aeafc6a
voomoo <rakib.7896@gmail.com>
2022-09-21 23:02:02 +0600

    33. Role Authorization

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -11,6 +11,7 @@ const {
     bootcampPhotoUpload,
 } = require("../controllers/bootcamps");
 const advancedResults = require("../middleware/advancedResults");
+const { protect, authorize } = require("../middleware/auth");
 const Bootcamp = require("../models/Bootcamp");
 
 //Include other resource routers
@@ -27,14 +28,16 @@ router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 router
     .route("/")
     .get(advancedResults(Bootcamp, "courses"), getBootcamps)
-    .post(createBootcamp);
+    .post(protect, authorize("publisher", "admin"), createBootcamp);
 
 router
     .route("/:id")
     .get(getBootcamp)
-    .put(updateBootcamp)
-    .delete(deleteBootcamp);
+    .put(protect, authorize("publisher", "admin"), updateBootcamp)
+    .delete(protect, authorize("publisher", "admin"), deleteBootcamp);
 
-router.route("/:id/photo").put(bootcampPhotoUpload);
+router
+    .route("/:id/photo")
+    .put(protect, authorize("publisher", "admin"), bootcampPhotoUpload);
 
 module.exports = router;
